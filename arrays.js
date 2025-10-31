0ae58504b45078cb8051a07a4dd0835f0f699f46
Liviwe Sandi <sandi.liviwe04@gmail.com>
2025-10-31 09:20:26 +0200

    Initial Ex_3 commit

---
+++ arrays.js
@@ -0,0 +1,7 @@
+let colours = [];
+colours.push("red", "green", "blue");
+let numbers = [3, 6, 9, 12, 15];
+colours.pop();
+colours.unshift("yellow")
+console.log("Length of colours array:", colours.length);
+console.log("Length of numbers array:", numbers.length);
